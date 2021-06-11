2e2c09d1e7cfec277659fd1d7bf892fcf04775be
Leonardo de Brito <leonardob@positivo.com.br>
2021-06-11 08:53:18 -0300

    implementing application logic

--- app/src/main/java/com/example/app_number_draw/MainActivity.java
+++ app/src/main/java/com/example/app_number_draw/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.app_number_draw;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void jogar(View view) {
+        TextView resultado = findViewById(R.id.txt2);
+        int x = new Random().nextInt(11);
+        resultado.setText("The number drawn was: " + x);
+    }
+
 }
